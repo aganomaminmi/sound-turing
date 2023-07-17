a9fc8c414ba9acce7157087367c71b735cea3751
Kohta Takanami <ripplekouha@gmail.com>
2023-07-17 22:22:05 +0900

    [add] outdir

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +11,9 @@ export default defineConfig({
     alias: [
       { find: '@/', replacement: '/src/' }
     ]
+  },
+  build: {
+    outDir: 'docs',
   },
   plugins: [react()],
 })
